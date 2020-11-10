1180f5077ac899eb22edd5f747b81b7195a410f3
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-11-10 11:24:29 +0100

    Refactors and adds third keyword

---
+++ src/expander-plugin-settings.ts
@@ -0,0 +1,17 @@
+export default class ExpanderPluginSettings {
+    public triggerOneValue: string;
+    public triggerOneKeyword: string;
+    public triggerTwoKeyword: string;
+    public triggerTwoValue: string;
+    public triggerThreeKeyword: string;
+    public triggerThreeValue: string;
+
+    constructor() {
+        this.triggerOneValue = "";
+        this.triggerOneKeyword = "";
+        this.triggerTwoKeyword = "";
+        this.triggerTwoValue = "";
+        this.triggerThreeKeyword = "";
+        this.triggerThreeValue = "";
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,116 @@
+import { Plugin } from 'obsidian';
+import ExpanderSettingTab from './expander-setting-tab';
+import ExpanderPluginSettings from './expander-plugin-settings';
+
+export default class ExpanderPlugin extends Plugin {
+
+	public settings: ExpanderPluginSettings;
+
+	private cmEditors: CodeMirror.Editor[];
+	private statusBar: HTMLElement;
+	private listening: boolean;
+
+	onload() {
+
+		this.loadSettings();
+		this.addSettingTab(new ExpanderSettingTab(this.app, this));
+
+		this.statusBar = this.addStatusBarItem()
+
+		this.cmEditors = [];
+		this.registerEvent(
+			this.app.on('codemirror', (cm: CodeMirror.Editor) => {
+				this.cmEditors.push(cm);
+				cm.on('keydown', this.handleKeyDown);
+			}),
+		);
+	}
+
+	onunload() {
+		console.log('unloading plugin');
+
+		this.cmEditors.forEach((cm) => {
+			cm.off('keydown', this.handleKeyDown);
+		});
+	}
+
+	private async loadSettings(): Promise<void> {
+		this.settings = new ExpanderPluginSettings();
+		(async () => {
+			const loadedSettings = await this.loadData();
+			if (loadedSettings) {
+				console.log('Found existing settings file');
+				this.settings.triggerOneKeyword = loadedSettings.triggerOneKeyword;
+				this.settings.triggerOneValue = loadedSettings.triggerOneValue;
+
+				this.settings.triggerTwoKeyword = loadedSettings.triggerTwoKeyword;
+				this.settings.triggerTwoValue = loadedSettings.triggerTwoValue;
+			} else {
+				console.log('No settings file found, saving...');
+				this.saveData(this.settings);
+			}
+		})();
+	}
+
+	private readonly handleKeyDown = (
+		cm: CodeMirror.Editor,
+		event: KeyboardEvent,
+	): void => {
+
+		if (!this.listening) {
+			if (event.key == ':') {
+				//see if this is the second :
+				let cursor = cm.getCursor();
+				let line = cursor.line;
+				let previousPosition = { ch: cursor.ch - 1, line: cursor.line, sticky: 'yes' }
+				let range = cm.getRange(previousPosition, cursor);
+
+				if ([':'].contains(range.charAt(0))) {
+					this.listening = true
+					this.statusBar.setText("I'm listening...");
+				}
+			}
+		} else if (event.key == 'Enter' || event.key == 'Tab' || event.key == ' ') {
+
+			let cursor = cm.getCursor();
+			let line = cursor.line;
+			let lineString = cm.getLine(line);
+
+			let patterns = new Map<string, string>();
+
+			// default triggers
+			patterns.set("::date", new Date().toDateString());
+
+			//custom triggers
+			if (this.settings.triggerOneKeyword) {
+				patterns.set("::" + this.settings.triggerOneKeyword, this.settings.triggerOneValue);
+			}
+
+			if (this.settings.triggerTwoKeyword) {
+				patterns.set("::" + this.settings.triggerTwoKeyword, this.settings.triggerTwoValue);
+			}
+			if (this.settings.triggerThreeKeyword) {
+				patterns.set("::" + this.settings.triggerThreeKeyword, this.settings.triggerThreeValue);
+			}
+
+			patterns.forEach((value: string, key: string) => {
+				const pattern = key;
+				const regex = RegExp(pattern);
+
+				if (regex.test(lineString)) {
+					let patternMatchIndex = lineString.match(pattern).index
+					let patternLength = pattern.length
+
+					cm.replaceRange(value, { ch: patternMatchIndex, line: line }, { ch: patternMatchIndex + patternLength, line: line });
+				}
+			});
+
+			this.listening = false
+			this.statusBar.setText("");
+
+		} else if (event.key == 'Escape') {
+			this.listening = false;
+			this.statusBar.setText("");
+		}
+	}
+}
