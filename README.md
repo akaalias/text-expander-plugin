51e0f8839593120f55bbe93e884a40ecbfb6afeb
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-11-10 12:04:49 +0100

    Adds updated readme and prettified code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A bare-bones proof-of-concept implementation of a simple text-expander in Obsidi
 2. Define your own custom trigger keyword like `foo`
 3. In the editor, type `::foo` and hit ENTER to expand
 
-Built-in is `::date` which will expand to todays date such as `Mon Nov 09 2020`
+Built-in is `::date` which will expand to todays date such as `Mon Nov 09 2020` and `::time` which will expand to your current timezone's time.
 
 ## Demo
 
@@ -16,6 +16,29 @@ Built-in is `::date` which will expand to todays date such as `Mon Nov 09 2020`
 
 ![Settings](https://github.com/akaalias/obsidian-text-expander-plugin/raw/main/settings.png)
 
-## Help
+## Hacking and Help
+This is mostly a proof of concept for myself and it's rather, erm, "pragmatic" code. If you see something you'd like to change, let me know. Here's how you can fiddle around on your own yourself and send a pull-request
 
-Feel free to clone and send pull requests!
+### Clone and make new branch
+- `cd $VAULT/.obsidian/plugins`
+- `git clone https://github.com/akaalias/text-expander-plugin`
+- `cd text-expander-plugin`
+- `git checkout -b my_branch`
+- `git remote add upstream https://github.com/akaalias/text-expander-plugin`
+
+### Install deps
+- `npm i && npm install -g prettier && npm install -g eslint`
+- `npm run dev`
+
+### Edit
+- `git checkout -b my_branch`
+- Make your edits
+- To check your changes, reload Obsidian (Command-R)
+
+### Commit-prep
+- `./ship.sh` will prettify and elsint once again
+- `git add -p` to stage changes individually
+- `git commit -m "my message"`
+
+### Publish pull request
+- `git push -u origin my_branch`
--- src/main.ts
+++ src/main.ts
@@ -3,127 +3,133 @@ import ExpanderSettingTab from "./expander-setting-tab";
 import ExpanderPluginSettings from "./expander-plugin-settings";
 
 export default class ExpanderPlugin extends Plugin {
-  public settings: ExpanderPluginSettings;
+	public settings: ExpanderPluginSettings;
 
-  private cmEditors: CodeMirror.Editor[];
+	private cmEditors: CodeMirror.Editor[];
 
-  private statusBar: HTMLElement;
+	private statusBar: HTMLElement;
 
-  private listening: boolean;
+	private listening: boolean;
 
-  public async onload(): Promise<void> {
-    this.loadSettings();
-    this.addSettingTab(new ExpanderSettingTab(this.app, this));
+	public async onload(): Promise<void> {
+		this.loadSettings();
+		this.addSettingTab(new ExpanderSettingTab(this.app, this));
 
-    this.statusBar = this.addStatusBarItem();
+		this.statusBar = this.addStatusBarItem();
 
-    this.cmEditors = [];
-    this.registerEvent(
-      this.app.on("codemirror", (cm: CodeMirror.Editor) => {
-        this.cmEditors.push(cm);
-        cm.on("keydown", this.handleKeyDown);
-      })
-    );
-  }
+		this.cmEditors = [];
+		this.registerEvent(
+			this.app.on("codemirror", (cm: CodeMirror.Editor) => {
+				this.cmEditors.push(cm);
+				cm.on("keydown", this.handleKeyDown);
+			})
+		);
+	}
 
-  public onunload(): void {
-    console.log("unloading plugin");
+	public onunload(): void {
+		console.log("unloading plugin");
 
-    this.cmEditors.forEach((cm) => {
-      cm.off("keydown", this.handleKeyDown);
-    });
-  }
+		this.cmEditors.forEach((cm) => {
+			cm.off("keydown", this.handleKeyDown);
+		});
+	}
 
-  private async loadSettings(): Promise<void> {
-    this.settings = new ExpanderPluginSettings();
-    (async () => {
-      const loadedSettings = await this.loadData();
-      if (loadedSettings) {
-        console.log("Found existing settings file");
-        this.settings.triggerOneKeyword = loadedSettings.triggerOneKeyword;
-        this.settings.triggerOneValue = loadedSettings.triggerOneValue;
+	private async loadSettings(): Promise<void> {
+		this.settings = new ExpanderPluginSettings();
+		(async () => {
+			const loadedSettings = await this.loadData();
+			if (loadedSettings) {
+				console.log("Found existing settings file");
+				this.settings.triggerOneKeyword = loadedSettings.triggerOneKeyword;
+				this.settings.triggerOneValue = loadedSettings.triggerOneValue;
 
-        this.settings.triggerTwoKeyword = loadedSettings.triggerTwoKeyword;
-        this.settings.triggerTwoValue = loadedSettings.triggerTwoValue;
-      } else {
-        console.log("No settings file found, saving...");
-        this.saveData(this.settings);
-      }
-    })();
-  }
+				this.settings.triggerTwoKeyword = loadedSettings.triggerTwoKeyword;
+				this.settings.triggerTwoValue = loadedSettings.triggerTwoValue;
 
-  private readonly handleKeyDown = (
-    cm: CodeMirror.Editor,
-    event: KeyboardEvent
-  ): void => {
-    if (!this.listening) {
-      if (event.key == ":") {
-        // see if this is the second :
-        const cursor = cm.getCursor();
-        const previousPosition = {
-          ch: cursor.ch - 1,
-          line: cursor.line,
-          sticky: "yes",
-        };
-        const range = cm.getRange(previousPosition, cursor);
+				this.settings.triggerThreeKeyword = loadedSettings.triggerThreeKeyword;
+				this.settings.triggerThreeValue = loadedSettings.triggerThreeValue;
+			} else {
+				console.log("No settings file found, saving...");
+				this.saveData(this.settings);
+			}
+		})();
+	}
 
-        if ([":"].contains(range.charAt(0))) {
-          this.listening = true;
-          this.statusBar.setText("I'm listening...");
-        }
-      }
-    } else if (event.key == "Enter" || event.key == "Tab" || event.key == " ") {
-      const cursor = cm.getCursor();
-      const { line } = cursor;
-      const lineString = cm.getLine(line);
+	private readonly handleKeyDown = (
+		cm: CodeMirror.Editor,
+		event: KeyboardEvent
+	): void => {
+		if (!this.listening) {
+			if (event.key == ":") {
+				// see if this is the second :
+				const cursor = cm.getCursor();
+				const previousPosition = {
+					ch: cursor.ch - 1,
+					line: cursor.line,
+					sticky: "yes",
+				};
+				const range = cm.getRange(previousPosition, cursor);
 
-      const patterns = new Map<string, string>();
+				if ([":"].contains(range.charAt(0))) {
+					this.listening = true;
+					this.statusBar.setText("I'm listening...");
+				}
+			}
+		} else if (event.key == "Enter" || event.key == "Tab" || event.key == " ") {
+			const cursor = cm.getCursor();
+			const { line } = cursor;
+			const lineString = cm.getLine(line);
 
-      // default triggers
-      patterns.set("::date", new Date().toDateString());
+			const patterns = new Map<string, string>();
 
-      // custom triggers
-      if (this.settings.triggerOneKeyword) {
-        patterns.set(
-          `::${this.settings.triggerOneKeyword}`,
-          this.settings.triggerOneValue
-        );
-      }
+			// default triggers
+			const d = new Date();
+			patterns.set("::date", d.toDateString());
+			patterns.set("::time", d.toLocaleTimeString());
 
-      if (this.settings.triggerTwoKeyword) {
-        patterns.set(
-          `::${this.settings.triggerTwoKeyword}`,
-          this.settings.triggerTwoValue
-        );
-      }
-      if (this.settings.triggerThreeKeyword) {
-        patterns.set(
-          `::${this.settings.triggerThreeKeyword}`,
-          this.settings.triggerThreeValue
-        );
-      }
+			// custom triggers
+			if (this.settings.triggerOneKeyword) {
+				patterns.set(
+					`::${this.settings.triggerOneKeyword}`,
+					this.settings.triggerOneValue
+				);
+			}
 
-      patterns.forEach((value: string, key: string) => {
-        const pattern = key;
-        const regex = RegExp(pattern);
+			if (this.settings.triggerTwoKeyword) {
+				patterns.set(
+					`::${this.settings.triggerTwoKeyword}`,
+					this.settings.triggerTwoValue
+				);
+			}
 
-        if (regex.test(lineString)) {
-          const patternMatchIndex = lineString.match(pattern).index;
-          const patternLength = pattern.length;
+			if (this.settings.triggerThreeKeyword) {
+				patterns.set(
+					`::${this.settings.triggerThreeKeyword}`,
+					this.settings.triggerThreeValue
+				);
+			}
 
-          cm.replaceRange(
-            value,
-            { ch: patternMatchIndex, line },
-            { ch: patternMatchIndex + patternLength, line }
-          );
-        }
-      });
+			patterns.forEach((value: string, key: string) => {
+				const pattern = key;
+				const regex = RegExp(pattern);
 
-      this.listening = false;
-      this.statusBar.setText("");
-    } else if (event.key == "Escape") {
-      this.listening = false;
-      this.statusBar.setText("");
-    }
-  };
+				if (regex.test(lineString)) {
+					const patternMatchIndex = lineString.match(pattern).index;
+					const patternLength = pattern.length;
+
+					cm.replaceRange(
+						value,
+						{ ch: patternMatchIndex, line },
+						{ ch: patternMatchIndex + patternLength, line }
+					);
+				}
+			});
+
+			this.listening = false;
+			this.statusBar.setText("");
+		} else if (event.key == "Escape") {
+			this.listening = false;
+			this.statusBar.setText("");
+		}
+	};
 }
